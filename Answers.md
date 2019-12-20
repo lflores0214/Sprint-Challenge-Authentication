08ce2e201ec339f3cf6f89de68ce0cd33d3c528e
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-12-20 09:24:02 -0700

    first commit

---
+++ Answers.md
@@ -0,0 +1,11 @@
+What is the purpose of using sessions?
+- Sessions allows data to be persisted over the server
+
+ What does bcrypt do to help us store passwords in a secure manner.
+ - It hashes the passwords 
+
+ What does bcrypt do to slow down attackers?
+ - It rehashes a given number of times so that it'll take longer to crack 
+
+ What are the three parts of the JSON Web Token?
+ - Header, payload, and signature
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,32 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Jokes = require("../jokes/jokes-model")
+
 
 router.post('/register', (req, res) => {
   // implement registration
+  const user = req.body
+  const hash = bcrypt.hashSync(user.password, 14);
+
+  user.password = hash;
+
+  Jokes.add(user)
+  .then(saved => {
+    res.status(201).json(saved)
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      errorMessage: "error registering user"
+    })
+  })
+
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  Jokes.findBy()
 });
 
 module.exports = router;
